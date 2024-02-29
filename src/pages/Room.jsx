66afd68fb86620d855bf290437883ef0e4a2d3f8
256bitguy <ashishrajsri2233@gmail.com>
2024-02-29 08:05:27 +0530

    added css file in index.css file

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,6 +1,7 @@
- import React, { useEffect } from 'react'
+ import React, { useEffect, useState } from 'react'
  import { databases ,DATABASE_ID,COLLECTION_ID_MESSSAGE} from '../appwriteconfig'
  const Room = () => {
+    const [messages,setMessages]=useState([])
      useEffect(()=>{
         getMessages()
      },[])
@@ -8,10 +9,27 @@
     const getMessages=async ()=>{
         const response=await databases.listDocuments(DATABASE_ID,COLLECTION_ID_MESSSAGE);
         console.log('RESPONSE:',response)
+        setMessages(response.documents)
     }
 
    return (
-     <div>'Room</div>
+     <div>
+        <div>
+            {
+                messages.map((message)=>{
+                     return(
+                        <div key={message.$id}>
+                        <div>
+                            <span>{message.$createdAt}</span>
+                            </div><div>
+                            <span>{message.body}</span>
+                            </div>
+                     </div>
+                     )
+                })
+            }
+        </div>
+     </div>
    )
  }
  
